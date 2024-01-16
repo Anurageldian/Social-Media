976df91f29ee7d4dd59f8167d559bb2e1e68b888
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-01-17 00:26:23 +0530

    Update brainly.js

--- funcs/brainly.js
+++ funcs/brainly.js
@@ -80,7 +80,7 @@ const Brainly = async (query, count = 10) => {
 };
 
 async function getBrainlyAnswer(bot, chatId, input, userName) {
-  if (!input) return bot.sendMessage(chatId, `Enter the question or question you want to search for in Brainly, for example\n/brainly what is the geographical location of India`);
+  if (!input) return bot.sendMessage(chatId, `Enter the question`);
   try {
     bot.sendChatAction(chatId, 'typing');
     let getdata = await Brainly(input, 10);
