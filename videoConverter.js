e541a1a2dae7caa192819170d545e3c7895be939
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-02-20 17:59:28 +0530

    Update and rename funcs/convert.js to videoConverter.js

---
+++ videoConverter.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const ffmpeg = require('fluent-ffmpeg');
+
+// Function to convert video to GIF using ffmpeg
+async function convertVideoToGif(videoPath) {
+  return new Promise((resolve, reject) => {
+    const gifPath = 'path/to/output.gif';
+
+    ffmpeg(videoPath)
+      .inputFormat('mp4') // Change this based on the input video format
+      .output(gifPath)
+      .on('end', () => {
+        console.log('Conversion finished');
+        resolve(gifPath);
+      })
+      .on('error', (err) => {
+        console.error('Error:', err);
+        reject(err);
+      })
+      .run();
+  });
+}
+
+module.exports = { convertVideoToGif };
