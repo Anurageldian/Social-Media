ea73e55a0d569155c4e8ab7d9b179758841d8dd6
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-06-13 19:47:38 +0530

    Update spotify.js

--- funcs/spotify.js
+++ funcs/spotify.js
@@ -22,7 +22,7 @@ https://api.spotifydown.com
 
 async function spotifyScraper(id, endpoint) {
   try {
-    let {data} = await axios.get(
+    let { data } = await axios.get(
       `https://api.spotifydown.com/${endpoint}/${id}`,
       {
         headers: {
@@ -43,16 +43,14 @@ async function getPlaylistSpotify(bot, chatId, url, userName) {
     let getdata = await spotifyScraper(`${pars.id}`, "trackList/playlist")
     let data = []
     getdata.trackList.map((maru) => {
-      data.push([{text: `${maru.title} - ${maru.artists}`,callback_data: "spt " + maru.id,}])
+      data.push([{ text: `${maru.title} - ${maru.artists}`, callback_data: "spt " + maru.id }])
     })
     let options = {
-      caption:
-        "Please select the music you want to download by pressing one of the buttons below!",
-      reply_markup: JSON.stringify({
-        inline_keyboard: data
-      })
-    };
-    await bot.sendPhoto(chatId,"https://telegra.ph/file/a41e47f544ed99dd33783.jpg",options);
+      caption: "Please select the music you want to download by pressing one of the buttons below!",
+      reply_markup: JSON.stringify({ inline_keyboard: data })
+    }
+    const imageUrl = getdata.metadata.image; // Use the playlist cover image URL
+    await bot.sendPhoto(chatId, imageUrl, options)
     await bot.deleteMessage(chatId, load.message_id)
   } catch (err) {
     await bot.sendMessage(String(process.env.DEV_ID), `[ ERROR MESSAGE ]\n\n• Username: @${userName}\n• File: funcs/spotify.js\n• Function: getPlaylistSpotify()\n• Url: ${url}\n\n${err}`.trim());
@@ -65,17 +63,20 @@ async function getAlbumsSpotify(bot, chatId, url, userName) {
   let load = await bot.sendMessage(chatId, "Loading, please wait.")
   try {
     let getdata = await spotifyScraper(`${pars.id}`, "trackList/album")
-    let data = [];
-    getdata.trackList.map((maru) => {data.push([{text: `${maru.title} - ${maru.artists}`,callback_data: "spt " + maru.id }])
+    let data = []
+    getdata.trackList.map((maru) => {
+      data.push([{ text: `${maru.title} - ${maru.artists}`, callback_data: "spt " + maru.id }])
     })
-    let options = { caption: "Please select the music you want to download by pressing one of the buttons below!",
-      reply_markup: JSON.stringify({inline_keyboard: data })
-    };
-    await bot.sendPhoto( chatId, "https://telegra.ph/file/a41e47f544ed99dd33783.jpg", options );
-    await bot.deleteMessage(chatId, load.message_id);
+    let options = {
+      caption: "Please select the music you want to download by pressing one of the buttons below!",
+      reply_markup: JSON.stringify({ inline_keyboard: data })
+    }
+    const imageUrl = getdata.metadata.image; // Use the album cover image URL
+    await bot.sendPhoto(chatId, imageUrl, options)
+    await bot.deleteMessage(chatId, load.message_id)
   } catch (err) {
     await bot.sendMessage(String(process.env.DEV_ID), `[ ERROR MESSAGE ]\n\n• Username: @${userName}\n• File: funcs/spotify.js\n• Function: getAlbumsSpotify()\n• Url: ${url}\n\n${err}`.trim());
-    return bot.editMessageText("Error getting playlist data!", { chat_id: chatId, message_id: load.message_id })
+    return bot.editMessageText("Error getting album data!", { chat_id: chatId, message_id: load.message_id })
   }
 }
 
@@ -90,30 +91,29 @@ async function getSpotifySong(bot, chatId, url, userName) {
         await bot.editMessageText(`Downloading song ${getdata.metadata.title} - ${getdata.metadata.artists}, please wait...`,{chat_id: chatId, message_id: load.message_id})
         let buff = await getBuffer(getdata.link)
         await fs.writeFileSync("content/" + fname, buff)
-        // let buf = await fs.readFileSync(`/tmp/${fname}`)
-        await bot.sendAudio(chatId, "content/" + fname, {caption: `Success download song ${getdata.metadata.title} - ${getdata.metadata.artists}`});
+        await bot.sendAudio(chatId, "content/" + fname, { caption: `Success download song ${getdata.metadata.title} - ${getdata.metadata.artists}` })
         await bot.deleteMessage(chatId, load.message_id)
         await fs.unlinkSync("content/" + fname)
       } else {
-        await bot.editMessageText("Error, failed to get data", {chat_id: chatId,message_id: load.message_id,})
+        await bot.editMessageText("Error, failed to get data", { chat_id: chatId, message_id: load.message_id })
       }
     } else {
       let getdata = await spotifyScraper(url, "download")
       let fname = `${filterAlphanumericWithDash(getdata.metadata.title)}-${filterAlphanumericWithDash(getdata.metadata.artists)}_${chatId}.mp3`
       if (getdata.success) {
-        await bot.editMessageText(`Downloading song ${getdata.metadata.title} - ${getdata.metadata.artists}, please wait...`,{chat_id: chatId, message_id: load.message_id })
+        await bot.editMessageText(`Downloading song ${getdata.metadata.title} - ${getdata.metadata.artists}, please wait...`, { chat_id: chatId, message_id: load.message_id })
         let buff = await getBuffer(getdata.link)
         await fs.writeFileSync("content/" + fname, buff)
-        await bot.sendAudio(chatId, "content/" + fname, {caption: `Success download song ${getdata.metadata.title} - ${getdata.metadata.artists}`});
+        await bot.sendAudio(chatId, "content/" + fname, { caption: `Success download song ${getdata.metadata.title} - ${getdata.metadata.artists}` })
         await bot.deleteMessage(chatId, load.message_id)
         await fs.unlinkSync("content/" + fname)
       } else {
-        await bot.editMessageText("Error, failed to get data", {chat_id: chatId,message_id: load.message_id })
+        await bot.editMessageText("Error, failed to get data", { chat_id: chatId, message_id: load.message_id })
       }
     }
   } catch (err) {
-    await bot.sendMessage(String(process.env.DEV_ID),`[ ERROR MESSAGE ]\n\n• Username: @${userName}\n• File: funcs/spotify.js\n• Function: getSpotifySong()\n• Url: ${url}\n\n${err}`.trim());
-    return bot.editMessageText("Failed to download song!", {chat_id: chatId, message_id: load.message_id })
+    await bot.sendMessage(String(process.env.DEV_ID), `[ ERROR MESSAGE ]\n\n• Username: @${userName}\n• File: funcs/spotify.js\n• Function: getSpotifySong()\n• Url: ${url}\n\n${err}`.trim());
+    return bot.editMessageText("Failed to download song!", { chat_id: chatId, message_id: load.message_id })
   }
 }
 
