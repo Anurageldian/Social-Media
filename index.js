d337882650559d788037999e99a4ee3796fbcfca
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-06-15 19:39:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -848,10 +848,6 @@ bot.onText(/\/info/, async (msg) => {
 
 
 
-const TelegramBot = require('node-telegram-bot-api');
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
-const bot = new TelegramBot(token, { polling: true });
-
 // Listen for photo messages
 bot.on('photo', (msg) => {
   const chatId = msg.chat.id;
