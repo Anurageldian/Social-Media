1bd63a9064a36f4082a81f562c9ac3e21decb436
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-06-15 17:03:01 +0530

    Create utils.js

---
+++ funcs/utils.js
@@ -0,0 +1,14 @@
+// utils.js
+
+// Utility function to format uptime
+function formatUptime(seconds) {
+  const days = Math.floor(seconds / (3600 * 24));
+  const hours = Math.floor(seconds % (3600 * 24) / 3600);
+  const minutes = Math.floor(seconds % 3600 / 60);
+  const uptimeString = `${days}d ${hours}h ${minutes}m`;
+  return uptimeString;
+}
+
+module.exports = {
+  formatUptime,
+};
