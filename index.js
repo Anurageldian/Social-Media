faf15efe78e435d9746646383314ae064f3f5740
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-06-16 18:54:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ bot.onText(/\/start/, async (msg) => {
 • <i>ꜱᴘᴏᴛɪꜰʏ</i>
 • <i>ɢɪᴛʜᴜʙ</i>\n
  ~~~~ ꜱʏꜱᴛᴇᴍ ᴜᴘᴛɪᴍᴇ: <b>${formattedUptime}</b> ~~~~ 
- ᴅᴀᴛᴇ & ᴛɪᴍᴇ ɪɴ ɪꜱᴛ: <b>${nowInIST}</b> `,
+ɪꜱᴛ: <b>${nowInIST}</b> `,
     reply_markup: { inline_keyboard: inlineKeyboard },
     parse_mode: 'HTML', // Ensure Markdown mode is enabled
   });
@@ -212,7 +212,7 @@ bot.onText(/\/start/, async (msg) => {
 
 ꜱᴇɴᴅ ɪᴍᴀɢᴇꜱ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜꜱᴇ ᴏᴄʀ (ᴇxᴛʀᴀᴄᴛ ᴛᴇxᴛ ᴏɴ ɪᴍᴀɢᴇ), ᴛᴇʟᴇɢʀᴀᴘʜ (ᴜᴘʟᴏᴀᴅ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ), ᴀɴᴅ ᴘᴏᴍꜰ2 (ᴜᴘʟᴏᴀᴅ ᴛᴏ ᴘᴏᴍꜰ-2)\n
 ~~~ ꜱʏꜱᴛᴇᴍ ᴜᴘᴛɪᴍᴇ: <b>${formattedUptime}</b> ~~~ 
- ᴅᴀᴛᴇ & ᴛɪᴍᴇ ɪɴ ɪꜱᴛ: <b>${nowInIST}</b> ~ `,
+ɪꜱᴛ: <b>${nowInIST}</b> ~ `,
         {
           chat_id: chatId,
           message_id: messageId,
@@ -245,7 +245,7 @@ bot.onText(/\/start/, async (msg) => {
 • <i>ꜱᴘᴏᴛɪꜰʏ</i>
 • <i>ɢɪᴛʜᴜʙ</i>\n
  ~~~~ ꜱʏꜱᴛᴇᴍ ᴜᴘᴛɪᴍᴇ: <b>${formattedUptime}</b> ~~~~ 
-  ᴅᴀᴛᴇ & ᴛɪᴍᴇ ɪɴ ɪꜱᴛ: <b>${nowInIST}</b> `,
+ɪꜱᴛ: <b>${nowInIST}</b> `,
         {
           chat_id: chatId,
           message_id: messageId,
