89076cec5ea70d32e81c51dc034a1e4ff3b8f318
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-06-16 17:30:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -933,7 +933,7 @@ bot.onText(/\/deletefiles/, async (msg) => {
 bot.onText(/\/dev/, async (msg) => { // Opening for the /dev command
   let chatId = msg.chat.id;
   if (String(msg.from.id) !== String(process.env.DEV_ID)) {
-    return;
+    return bot.deleteMessage(msg.chat.id, msg.message_id);
   }
  const uptimeSeconds = os.uptime();
   const formattedUptime = formatUptime(uptimeSeconds); // Use the formatUptime function from utils.js
