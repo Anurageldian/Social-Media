d695210b09ab1e5533f529b3347280cb72435535
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-07-01 23:13:08 +0530

    Update ai.js

--- funcs/ai.js
+++ funcs/ai.js
@@ -5,7 +5,7 @@ async function getAiResponse(bot, chatId, input, userName) {
   if (!input) return bot.sendMessage(chatId, `Enter your question, example\n/ai what is javascript`);
   try {
     bot.sendChatAction(chatId, 'typing');
-    let { data } = await axios(`https://chatgpt-api.shn.hk/v1/`, {
+    let { data } = await axios(`http://localhost:3040/v1/chat/completions`, {
       method: "post",
       data: {
         botId: "default",
