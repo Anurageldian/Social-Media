61ec62fcb1ecab7fdf3c9ca9e78ef533971841d1
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-10-09 15:34:26 +0530

    Update google.js

--- funcs/google.js
+++ funcs/google.js
@@ -1,23 +1,23 @@
-// require('dotenv').config()
-// const { search } = require('google-sr');
-// const logChannelId = process.env.LOGC_ID;
+require('dotenv').config()
+const { search } = require('google-sr');
+const logChannelId = process.env.LOGC_ID;
 
-// async function googleSearch(bot, chatId, query, userName) {
-//   if (!query) return bot.sendMessage(chatId, `Enter your Google search query, example\n/google what is javascript`)
-//   bot.sendChatAction(chatId, 'typing');
-//   try {
-//     const searchResults = await search({ query: query });
-//     let resultS = `GOOGLE SEARCH\n\n`
-//     for (let i = 0;i < 5;i++) {
-//       resultS += `• Title: ${searchResults[i].title}\n• Link: ${searchResults[i].link}\n• Description: ${searchResults[i].description}\n\n`
-//     };
-//     return bot.sendMessage(chatId, resultS);
-//   } catch (err) {
-//     await bot.sendMessage(logChannelId, `[ ERROR MESSAGE ]\n\n• Username: @${userName}\n• File: funcs/google.js\n• Function: googleSearch()\n• Input: ${query}\n\n${err}`.trim());
-//     return bot.sendMessage(chatId, 'An error occurred!');
-//   }
-// }
+async function googleSearch(bot, chatId, query, userName) {
+  if (!query) return bot.sendMessage(chatId, `Enter your Google search query, example\n/google what is javascript`)
+  bot.sendChatAction(chatId, 'typing');
+  try {
+    const searchResults = await search({ query: query });
+    let resultS = `GOOGLE SEARCH\n\n`
+    for (let i = 0;i < 5;i++) {
+      resultS += `• Title: ${searchResults[i].title}\n• Link: ${searchResults[i].link}\n• Description: ${searchResults[i].description}\n\n`
+    };
+    return bot.sendMessage(chatId, resultS);
+  } catch (err) {
+    await bot.sendMessage(logChannelId, `[ ERROR MESSAGE ]\n\n• Username: @${userName}\n• File: funcs/google.js\n• Function: googleSearch()\n• Input: ${query}\n\n${err}`.trim());
+    return bot.sendMessage(chatId, 'An error occurred!');
+  }
+}
 
-// module.exports = {
-//   googleSearch
-// }
+module.exports = {
+  googleSearch
+}
