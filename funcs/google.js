16af760a5bf6c8f96328828ca0eea1dbe16b8a64
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-10-09 15:34:14 +0530

    Update google.js

--- funcs/google.js
+++ funcs/google.js
@@ -21,32 +21,3 @@
 // module.exports = {
 //   googleSearch
 // }
-require('dotenv').config();
-import google from 'google-that';
-const logChannelId = process.env.LOGC_ID;
-
-async function googleSearch(bot, chatId, query, userName) {
-  if (!query) {
-    return bot.sendMessage(chatId, `Enter your Google search query, example\n/google what is javascript`);
-  }
-
-  bot.sendChatAction(chatId, 'typing');
-
-  try {
-    const searchResults = await google({ query: query });
-    let resultS = `GOOGLE SEARCH\n\n`;
-
-    for (let i = 0; i < Math.min(5, searchResults.length); i++) {  // Limit to 5 results
-      resultS += `• Title: ${searchResults[i].title}\n• Link: ${searchResults[i].link}\n• Description: ${searchResults[i].snippet}\n\n`;
-    }
-
-    return bot.sendMessage(chatId, resultS);
-  } catch (err) {
-    await bot.sendMessage(logChannelId, `[ ERROR MESSAGE ]\n\n• Username: @${userName}\n• File: funcs/google.js\n• Function: googleSearch()\n• Input: ${query}\n\n${err}`.trim());
-    return bot.sendMessage(chatId, 'An error occurred!');
-  }
-}
-
-module.exports = {
-  googleSearch
-};
