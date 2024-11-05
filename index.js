0bee241823d83609b417abe328ae6190902c72cf
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-11-05 17:33:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -975,6 +975,8 @@ bot.onText(/\/ban (.+)/, async (msg, match) => {
       bot.sendMessage(chatId, respo, { parse_mode: 'HTML' });
     } catch (error) {
       console.error('Error banning user:', error.message);
+      const respo = `Failed to Ban User <a href="tg://user?id=${userIdToBan}">${userFullName}</a> ${userUsername}`;
+      bot.sendMessage(chatId, respo, { parse_mode: 'HTML' });
       bot.sendMessage(chatId, `Failed to ban user ${userIdOrUsernameToBan}.`);
     }
   } catch (error) {
