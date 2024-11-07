27a4c827e72c3064817265384c34d384772828f8
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-11-07 15:17:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -688,9 +688,9 @@ bot.on('message', async (msg) => {
       (setting === 'pin' && isPinnedMessage) ||
       (setting === 'title' && isTitleChange) ||
       (setting === 'videochat' && isVideoChatMessage)
-    ) {
-      await bot.deleteMessage(chatId, msg.message_id);
-    }
+    // ) {
+    //   await bot.deleteMessage(chatId, msg.message_id);
+    // }
   } catch (error) {
     console.error('Error deleting service message:', error);
   }
