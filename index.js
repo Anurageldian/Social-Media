0e4e6b601aefeab60cc8734f1ec754f29233902c
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-11-07 17:57:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1002,7 +1002,14 @@ bot.onText(/\/free (\d+)/, async (msg, match) => {
 
   // Unmute the user
   bot.restrictChatMember(chatId, targetUserId, {
-    can_send_messages: true,
+        can_send_messages: true,
+        can_send_media_messages: true,
+        can_send_polls: true,
+        can_send_other_messages: true,
+        can_add_web_page_previews: true,
+        can_change_info: true,
+        can_invite_users: true,
+        can_pin_messages: true
   });
 
   bot.sendMessage(chatId, `User ${targetUserId} has been unlocked.`);
