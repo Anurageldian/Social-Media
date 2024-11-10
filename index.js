15b5985e5250a7165552a0d1b6522d407e90c30c
Anurag <126282709+Anurageldian@users.noreply.github.com>
2024-11-10 16:26:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2875,7 +2875,7 @@ bot.onText(/\/info(?: (\d+))?/, async (msg, match) => {
 
     // Check if the target user is the developer
     if (targetUserId == DEV_ID) {
-      caption += '➻ ᴛʜɪs ᴜsᴇʀ ɪs ᴍʏ ᴏᴡɴᴇʀ';
+      caption += '\n➻ ᴛʜɪs ᴜsᴇʀ ɪs ᴍʏ ᴏᴡɴᴇʀ';
     }
 
     // If the target user has a profile photo, send it with the caption
